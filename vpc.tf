ea436be17e56078edf92c3f9b23058b5d5291d8a
vikas99341 <48900898+vikas99341@users.noreply.github.com>
2023-02-10 08:26:45 +0530

    updated vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -2,6 +2,6 @@ resource "aws_vpc" "demo_vpc" {
   cidr_block       = "${var.vpc_cidr}"
   instance_tenancy = "${var.tenancy}"
   tags = {
-    Name = "${terraform.workspace}-javahome-vpc"
+    Name = "${terraform.workspace}-demo-vpc"
   }
 }
